2a8bc6dd3cc8fccdaff13ad4496de224fa6997fa
shiv_bilgundi <shivbilgundiyt@gmail.com>
2024-02-01 14:49:05 +0530

    step4 with step3 solution

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // Write your js code...
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+const qrCont = document.querySelector(".qr-img");
+const qrImg = document.querySelector(".qr-img img");
